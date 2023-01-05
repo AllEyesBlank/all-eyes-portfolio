beafe8d61d334b42a7839d1ce04ae33f76e5338c
Alice Blank <alicejblank@gmail.com>
2023-01-05 11:06:22 -0600

    added about modal

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import aboutImage from './../images/IMG_5948 copy.jpg';
+import AboutModal from './AboutModal.jsx';
 
 const About = () => {
+  const [show, setShow] = useState(false)
   return (<div className="about-wrapper">
     <span className="about-text-wrapper">
+      {show ? <AboutModal /> : <></>}
       <div className="about-title">Hi-- it's me, Alice!</div>
       <div className="about-description">I'm a full stack software engineer, and it's nice to meet you!</div>
+      <div className="about-button" onClick={() => setShow(!show)}>(read the whole story)</div>
     </span>
       <div className="about-image"><img src={aboutImage}></img></div>
     </div>)
---
+++ src/AboutModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutModal = () => {
+  return(<div className="about-modal-wrapper">
+    <div className="about-modal">
+      <div className="about-modal-content">
+      <p>When I was asked to be the keynote speaker at the LGBT Rainbow Banquet for Syracuse University back in my days as an undergraduate, I was introduced as being a "genuine and gentle person", a compliment that I have been striving to live up to ever since. I have a downright voracious appetite for learning and knowledge-- as my tenure in educational fields showcases, as well as my pursuit of higher degrees and certificates-- and I believe that the greatest way to indicate true understanding is to be able to lift up others in their understanding. With my years as a teaching assistant in academia complemented by my hard work in the service industry as a barista, shift lead and eventual assistant manager of various cafes, I hope my background inspires a sense of stability and discipline. More than a strong work ethic, though, I hope my leading quality is a generosity of spirit in helping teams achieve their goals.</p>
+
+      <p>I turned to software engineering as I grew hungry for a new venue at which to throw my passion towards problem-solving and critical thinking. While I love literature and am grateful for my time studying it, I have found that nothing is quite so rewarding as thinking through and implementing elegant solutions to difficult technical problems, software engineering truly being one of the most hands-on ways to tackle the architecture of our modern world as it becomes increasingly tech-forward. I feel so lucky to be in this industry and have the opportunity to work on all scales of solutions.</p>
+      </div>
+    </div>
+  </div>)
+}
+
+export default AboutModal;
